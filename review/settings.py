86581002f62ad161008510001744b86560103ced
Itz-Caseay <gred01485@gmail.com>
2026-08-05 19:16:11 +0100

    latest1

--- review/settings.py
+++ review/settings.py
@@ -128,7 +128,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'faithprince32@gmail.com'
-EMAIL_HOST_PASSWORD = 'yogxbkguxsgacmnt'
+EMAIL_HOST_PASSWORD = 'ajmzqffucsvkbquy'
 DEFAULT_FROM_EMAIL = 'faithprince32@gmail.com'
 CONTACT_EMAIL = 'faithprince32@gmail.com'
 
